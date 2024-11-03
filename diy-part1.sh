cd652c41e61ee13a6a7c80fc8dc5833a8be103ab
piaoyizy123 <50651117+piaoyizy123@users.noreply.github.com>
2024-11-03 12:54:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,3 +9,12 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+
+sed -i 's/192.168.1.1/10.10.10.6/g' package/base-files/luci2/bin/config_generate
+
+rm -rf feeds/luci/luci-app-smartdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
