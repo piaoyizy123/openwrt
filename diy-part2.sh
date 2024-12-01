afaee997a8c02a8625599311cd15f2fe31f904a2
piaoyizy123 <50651117+piaoyizy123@users.noreply.github.com>
2024-12-01 17:47:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,9 +11,8 @@
 #
 
 rm -rf package/feeds/luci/luci-app-passwall/
-rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
-
-
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,chinadns-ng}
+git clone https://github.com/kenzok8/small package/helloworld
+sed -i 's/api.is_finded("fw3")/api.is_finded("fw30")/g' package/helloworld/luci-app-passwall/luasrc/model/cbi/passwall/client/other.lua
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.6/g' package/base-files/files/bin/config_generate
